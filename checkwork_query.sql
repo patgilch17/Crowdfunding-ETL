0ce90fe6061532035a661d78e40f2d20ddf31d20
patgilch17 <patrickgilchrist@Patricks-MacBook-Air.local>
2022-12-11 19:32:12 -0700

    completed Module work before HW

---
+++ checkwork_query.sql
@@ -0,0 +1,20 @@
+-- Added these queries to check my work
+select * from campaign;
+
+select * from category;
+
+select * from subcategory;
+
+select * from contacts;
+
+/*
+drop table campaign;
+drop table category cascade;
+drop table subcategory cascade;
+drop table contacts cascade;
+
+If any of these are dropped you will need to rebuild from the schema file
+
+
+*/
+
